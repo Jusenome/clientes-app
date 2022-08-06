03e79f53f08466d9c02af2f8f20b075b02259ec7
Jusenome3 <sebastian.noriega@idelity.net>
2022-08-06 07:57:49 -0500

    Inicio

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from "./cliente";
+
+
+
+export const Clientes: Cliente[] = [
+
+    {id: 1, nombre: 'Sebastian', apellido: 'Noriega', createAt: '2022-07-30', email: 'sebastian.noriega@idelity.co'}
+
+  ];
